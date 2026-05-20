9c45d0821b94ea4808e5b57c35baf99b3e2920da
jrigottiuade <jrigotti@uade.edu.ar>
2026-05-20 17:12:55 -0300

    update clsae 10

---
+++ Unidad_9_1/_2_codigo_clase.py
@@ -0,0 +1,10 @@
+
+"""
+Try-except articles
+https://medium.com/p/97d12aac91b6
+https://medium.com/@syaokifaradisa09/python-exception-handling-making-your-code-bulletproof-a347f306b03a
+https://medium.com/@vivekmcm1/python-exception-handling-explained-with-real-world-examples-15807183af6e
+https://medium.com/stackademic/junior-devs-use-try-catch-everywhere-senior-devs-use-these-4-exception-handling-patterns-dcd869ed6551
+https://medium.com/@chandantechie/top-10-ways-to-handle-exception-in-python-with-coding-examples-6de53cae5338
+"""
+
